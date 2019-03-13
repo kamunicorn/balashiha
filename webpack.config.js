7981468403d7642fafb71cff4b21b7697907635f
vera <vera.kamunicorn@gmail.com>
2019-03-13 14:29:35 +1200

    Пыталась настроить Babel для IE, но не получилось. Подключила просто полифиллы в хэде условным комментарием

--- webpack.config.js
+++ webpack.config.js
@@ -17,29 +17,11 @@ module.exports = {
   devtool: 'source-map',
 
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader?optional[]=runtime',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                  targets: {
-                    // browsers: ['last 2 versions', 'ie >= 10']}}]]}}}]},/*
-                    edge: "17",
-                    firefox: "60",
-                    chrome: "67",
-                    safari: "11.1",
-                    ie: "10"
-                }//,useBuiltIns: "usage"
-              }]
-            ]
-          }
-        }
-      }
-    ]
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }]
   },
   plugins: [
     new UglifyJsPlugin()
